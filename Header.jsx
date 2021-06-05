bd6bb7411a3bfae44b6838a31a72961c3ca75713
kn2822 <84955316+kn2822@users.noreply.github.com>
2021-06-05 16:43:38 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> Kn </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> ReactJS </h1>
+      <p>
+        This is an amazing bootcamp taken by shaurya sinha and you can learn
+        about reactJS
+      </p>
+    </div>
+  );
+}
+
+export default Note;
